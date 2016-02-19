3c666091abf53378e066551431528fd8b877aa84
Matt English <menglish78@bellsouth.net>
2016-02-18 20:37:33 -0500

    Added a back to top button. Added a soundcloud player.

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,19 @@ $(document).on('click', 'a[href^="#"]', function(e) {
     var pos = $(id).offset().top;
     $('body, html').animate({scrollTop: pos});
 });
+
+// Back to Top Button
+$(document).ready(function() {
+			$(window).scroll(function() {
+				if ($(this).scrollTop() > 200) {
+					$('.go-top').fadeIn(200);
+				} else {
+					$('.go-top').fadeOut(200);
+				}
+			});
+			$('.go-top').click(function(event) {
+				event.preventDefault();
+
+				$('html, body').animate({scrollTop: 0}, 300);
+			})
+		});
